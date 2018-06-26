bfd91c49e5368814c5bbb53cc0d448f45f5a00d6
LeesimLoveXz <565048350@qq.com>
2018-06-26 10:47:28 +0800

    添加手势侧滑功能

--- LMWebview/LMWebviewController.m
+++ LMWebview/LMWebviewController.m
@@ -15,17 +15,40 @@
 
 #define LMRGBAColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
 
-@interface LMWebviewController ()<WKNavigationDelegate,WKUIDelegate>
+@interface LMWebviewController ()<WKNavigationDelegate,WKUIDelegate,UIGestureRecognizerDelegate>
 
 @property (nonatomic,strong) WKWebView * webview;
 @property (nonatomic,strong) UIBarButtonItem * customBackBarItem;
 @property (nonatomic,strong) UIBarButtonItem * closeButtonItem;
 @property (nonatomic,strong)UIBarButtonItem* refreshBarItem;
-@property (nonatomic,strong) UIView * progreesBackView;
 @property (nonatomic,strong) UIView * progressGetView;
+//为了
+@property (nonatomic,assign) id delegate;
+
+//注入方法名 用于接受JS调用原生方法 让Web端掉用iOS端代码
+@property (nonatomic,strong) WKWebViewConfiguration *webConfig;
+
 @end
 
 @implementation LMWebviewController
+
+//由于，手动设置了导航栏左按钮，因此我们会发现系统自带的侧滑返回功能失效了，为了网页所在视图控制器的侧滑返回功能可实现，需要再修复其失效的问题。
+#pragma mark - 让自定义的导航栏左侧按钮支持侧滑手势的处理
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    if (self.navigationController.viewControllers.count > 1) {
+        //记录原来的代理
+        self.delegate = self.navigationController.interactivePopGestureRecognizer.delegate;
+        //修复手势操作代理
+        self.navigationController.interactivePopGestureRecognizer.delegate = self;
+    }
+}
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    //把手势代理在传递给原来的代理对象
+    self.navigationController.interactivePopGestureRecognizer.delegate = self.delegate;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -35,11 +58,10 @@
     
     [self.view addSubview:self.webview];
     [self.refreshBarItem class];
-    [self.view addSubview:self.progreesBackView];
-    [self.progreesBackView addSubview:self.progressGetView];
+    [self.view addSubview:self.progressGetView];
     
     
-    //更新右侧按钮
+    //更新左侧按钮
     [self updateNavigationItems];
     
 }
@@ -58,19 +80,16 @@
 {
     //判断左侧按钮状态
     [self updateNavigationItems];
-
 }
 
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     
     if ([keyPath isEqualToString:@"estimatedProgress"]) {
-        
         if (object == self.webview) {
- 
-            self.progreesBackView.hidden = NO;
+            self.progressGetView.hidden = NO;
             [UIView animateWithDuration:0.1 delay:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
-                self.progressGetView.frame =CGRectMake(0,0,SCREEN_WIDTH*self.webview.estimatedProgress, 3);
+                self.progressGetView.frame =CGRectMake(0,NAV_HEIGHT,SCREEN_WIDTH*self.webview.estimatedProgress, 3);
             } completion:nil];
             //下面动画是为了防止加载进度在快速请求另外的网页的时候
             //出现进度条回缩的问题
@@ -78,21 +97,25 @@
                 [UIView animateWithDuration:0.3 delay:0.3 options:UIViewAnimationOptionCurveEaseOut animations:^{
 
                 } completion:^(BOOL finished) {
-                    self.progressGetView.frame =CGRectMake(0,0,0, 3);
-                    self.progreesBackView.hidden = YES;
+                    self.progressGetView.frame =CGRectMake(0,NAV_HEIGHT,0, 3);
+                    self.progressGetView.hidden = YES;
                 }];
             }
         }
-        
     }else if ([keyPath isEqualToString:@"title"])
     {
-        
         if (object == self.webview) {
             self.title = self.webview.title;
         }
     }
 }
 
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    return self.navigationController.viewControllers.count > 1;
+}
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return self.navigationController.viewControllers.count > 1;
+}
 
 
 #pragma mark - pravite method
@@ -183,6 +206,9 @@
         _webview = [[WKWebView alloc]initWithFrame:CGRectMake(0, NAV_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT-NAV_HEIGHT)];
         _webview.navigationDelegate = self;
         _webview.UIDelegate = self;
+        //侧滑返回上层
+        _webview.allowsBackForwardNavigationGestures = YES;
+
         //观察进度变化
         [_webview addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionOld context:NULL];
         //观察网页标题
@@ -191,22 +217,12 @@
     return _webview;
 }
 
--(UIView *)progreesBackView
-{
-    if (!_progreesBackView) {
-        _progreesBackView = [[UIView alloc]init];
-        _progreesBackView.frame = CGRectMake(0, NAV_HEIGHT,SCREEN_WIDTH, 3);
-        _progreesBackView.backgroundColor = LMRGBAColor(238, 238, 238, 1);
-    }
-    return _progreesBackView;
-}
-
 -(UIView *)progressGetView
 {
     if (!_progressGetView) {
         _progressGetView = [[UIView alloc]init];
-        _progressGetView.frame =CGRectMake(0, 0,SCREEN_WIDTH , 0);
-        _progressGetView.backgroundColor = LMRGBAColor(255, 45, 85, 1);
+        _progressGetView.frame =CGRectMake(0, NAV_HEIGHT,0 , 3);
+        _progressGetView.backgroundColor = LMRGBAColor(67, 138, 230, 1);
     }
     return _progressGetView;
 }
